d3267cc5790191effc7c642eb52655406de9e4a3
itsmarrok <manpreet1995@hotmail.de>
2018-02-14 16:02:34 +0100

    added new files

---
+++ src/main/java/de/pro/Propra/PropraApplication.java
@@ -0,0 +1,12 @@
+package de.pro.Propra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropraApplication.class, args);
+	}
+}
